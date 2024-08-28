58070b1ab7059063fe87699e1d3cdebf775f1580
Eduardo Sena <dudu94bhz@gmail.com>
2024-08-28 16:55:45 -0300

    Configurando Entity Framework

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_api_fuel_manager.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {                
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo>  Consumos { get; set; }   
+    }
+}
