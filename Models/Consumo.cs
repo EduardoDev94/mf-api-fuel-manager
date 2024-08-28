9f97b980cc34b57c57aa1ade3a11095d89960bd0
Eduardo Sena <dudu94bhz@gmail.com>
2024-08-28 16:07:48 -0300

    Criação e Configuração dos modelos de Veiculo e Consumo

---
+++ Models/Consumo.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_api_fuel_manager.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+        [Required]
+        public int VeiculoId { get; set; }//Referência da chave estrangeira de Veiculo sendo 1 veículo para muitos consumos
+
+        public Veiculo Veiculo { get; set; }
+
+    }
+    public enum TipoCombustivel
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
